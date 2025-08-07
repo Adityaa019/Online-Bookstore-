aa0e3f0334ec656b53c1434f02335e1cbb4e9fce
Adityaa019 <adityatandon019@gmail.com>
2025-08-08 01:19:25 +0530

    Update SQLQuery1.sql

--- SqlQueries/SQLQuery1.sql
+++ SqlQueries/SQLQuery1.sql
@@ -3,4 +3,4 @@ select u.Name,u.UserName,r.Name, c.Name
 from AspNetUserRoles ur
 join AspNetUsers u on u.Id = ur.UserId
 join AspNetRoles r on r.Id = ur.RoleId
-left join Companies c on c.Id = u.CompanyId
+left join Companies c on c.Id = u.CompanyIdd
